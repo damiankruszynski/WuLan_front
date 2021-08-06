61affd28b6bbb3691486a395d2e8aa25e7784eaf
Damian Kruszynski <d.damiankruszynski@gmail.com>
2021-08-06 20:53:29 +0200

    commit

--- src/Home/services/HomeService.js
+++ src/Home/services/HomeService.js
@@ -13,6 +13,14 @@ class HomeService {
         return axios.get(consts.getAUTH_API_BASE_URL() + 'files', token);
     };
 
+        getMovie(path) {
+            let token = {
+                headers: authHeader(),
+                params: path
+            };
+            return axios.get(consts.getAUTH_API_BASE_URL() + 'stream', token);
+        };
+
 }
 
 export default new HomeService()
